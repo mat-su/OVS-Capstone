354ff151979b2350f87edd7261ab26e825024aa8
mat-su <mrtangpus@gmail.com>
2022-03-21 11:51:36 +0800

    added another filter in sql query whenever retrieving a candidate

--- subadmin/fetchdata.php
+++ subadmin/fetchdata.php
@@ -1,12 +1,13 @@
 <?php
-
+session_start();
 include '../functions.php';
 $conn = MYSQL_DB_Connection();
-
+$org_id = $_SESSION['sa_org_id'];
 if (isset($_POST['post_btn']) && isset($_POST['studnum'])) {
     $studnum = $_POST['studnum'];
-    $stmt = $conn->prepare("SELECT enr_fname AS fname, enr_mname AS mname, enr_lname AS lname FROM tbl_enr_stud WHERE enr_studnum = :studnum");
+    $stmt = $conn->prepare("SELECT enr_fname AS fname, enr_mname AS mname, enr_lname AS lname, o.org_name FROM tbl_enr_stud e JOIN tbl_course c ON e.enr_course = c.course JOIN tbl_stud_orgs o ON c.id = o.org_course_id WHERE enr_studnum = :studnum and o.org_id = :org_id");
     $stmt->bindParam(':studnum', $studnum, PDO::PARAM_STR);
+    $stmt->bindParam(':org_id', $org_id, PDO::PARAM_INT);
     $stmt->execute();
     $student = $stmt->fetch(PDO::FETCH_ASSOC);
     if ($student) {
@@ -33,7 +34,7 @@ if (isset($_POST['post_btn']) && isset($_POST['studnum'])) {
     } else {
         ?>
         <script>$('#staticBackdropCreate button[form="frm_create"]').prop('disabled', true);</script>
-        <div id="response"><em style="color: red;">Retrieve nothing</em></div>
+        <div id="response"><small><em style="color: red;"><b>Retrieve nothing</b></em>: The student number you searched does not exist in this student organization.</small></div>
         <div class="row">
             <div class="col-md-6">
                 <label for="fname" class="form-label">First Name</label>
