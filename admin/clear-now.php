717048f61acf9a51a6b7280c7542b70ac5810f32
mat-su <mrtangpus@gmail.com>
2022-04-02 00:52:55 +0800

    fixed sql query for resetting tally and voter status

--- admin/clear-now.php
+++ admin/clear-now.php
@@ -6,13 +6,14 @@ if (isset($_POST["id"])) {
     WHERE vs_org_id = :id");
     $stmt->bindParam(':id', $_POST["id"], pdo::PARAM_STR);
     $stmt->execute();
-    $stmt = $conn->prepare("DELETE tbl_tally, tbl_voter_status
-    FROM tbl_voter 
-    LEFT JOIN tbl_tally ON tbl_voter.v_id = tbl_tally.v_id
-    LEFT JOIN tbl_voter_status ON tbl_tally.v_id = tbl_voter_status.v_id
-    LEFT JOIN tbl_course ON tbl_voter.v_course = tbl_course.course
-    LEFT JOIN tbl_stud_orgs ON tbl_course.id = tbl_stud_orgs.org_course_id
-    WHERE tbl_tally.v_id IS NOT NULL AND tbl_stud_orgs.org_id = :id");
+    $stmt = 
+        $conn->prepare("DELETE tbl_tally, tbl_voter_status FROM tbl_voter_status
+            LEFT JOIN tbl_voter ON tbl_voter_status.v_id = tbl_voter.v_id
+            LEFT JOIN tbl_enr_stud ON tbl_voter.v_studnum = tbl_enr_stud.enr_studnum 
+            LEFT JOIN tbl_course ON tbl_course.course = tbl_voter.v_course
+            LEFT JOIN tbl_stud_orgs ON tbl_course.id = tbl_stud_orgs.org_course_id
+            LEFT JOIN tbl_tally ON tbl_tally.v_id = tbl_voter.v_id 
+            WHERE tbl_stud_orgs.org_id = :id");
     $stmt->bindParam(':id', $_POST["id"], pdo::PARAM_STR);
     $stmt->execute();
     $resp['status'] = "success";
