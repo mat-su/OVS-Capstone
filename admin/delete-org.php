44c6136c44e9fd6732afbd7a2445b5945730af73
mat-su <mrtangpus@gmail.com>
2022-04-02 00:48:24 +0800

    removed unnecessary files

--- admin/delete-org.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-if (isset($_POST["org_id"])) {
-    $output = '<form id="trash" method="post" action="trash-org.php"> <p> Are you sure you want to delete this student organization? </p><input type="text" class="form-control mb-2" placeholder="" name="id" value="'.$_POST["org_id"].'" hidden></form>';
-    echo "$output";
-}
